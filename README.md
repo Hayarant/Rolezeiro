9279fc57dd1b23d28e2bc1d5aa1d02ecbdb39871
João <hayarant@gmail.com>
2022-05-09 14:40:19 -0300

    Inicial
    Adicionado Token;
    Adicionado Dependências;
    Adicionado Código Base;

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Rolezeiro
 Bot de discord para marcar , lembrar e confirmar encontros entre os membros.
+
+# Install
+# npm install --save discord.js dotenv
+
+# Run 
+# node index.js
---
+++ index.js
@@ -0,0 +1,19 @@
+// Carrega o arquivo .env que contem o Token do Bot
+require('dotenv').config();
+
+const Discord = require('discord.js');
+const {Intents} = require('discord.js');
+const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+//Realiza o login do bot com o Token do arquivo .env
+client.login(process.env.DISCORD_TOKEN);
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+      msg.reply('pong');
+    }
+  });
